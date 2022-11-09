f4caa896861d1cc2a485248438ee6915e1cacbe5
AndrePim <andreo.obolensky@gmail.com>
2022-11-09 22:43:45 +0300

    End Lesson, add and test gitignore

--- firstsite/main/views.py
+++ firstsite/main/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def index(request):
@@ -12,4 +13,18 @@ def about(request):
 
 
 def create(request):
-    return render(request, 'main/create.html')
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST) 
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Форма была неверной'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
